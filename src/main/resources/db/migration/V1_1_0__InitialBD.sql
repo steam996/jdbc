e429b12e02e43875df3998b66ce81f16b79e6205
Boris Andros <boris.andros96@gmail.com>
2023-05-07 02:38:22 +0300

    Добавлены миграции

--- src/main/resources/db/migration/V1_1_0__InitialBD.sql
+++ src/main/resources/db/migration/V1_1_0__InitialBD.sql
@@ -1,15 +1,15 @@
-create schema if not exists CUSTOMERS;
+create schema CUSTOMERS;
 
-create table if not exists customers.CUSTOMERS
+create table  customers.CUSTOMERS
 (
     id           serial primary key,
     name         varchar(50),
     surmane      varchar(50),
     age          int8,
     phone_number varchar(50)
-);
+    );
 
-create table if not exists customers.orders
+create table  customers.orders
 (
     id           serial primary key,
     date         date,
@@ -17,4 +17,4 @@ create table if not exists customers.orders
     product_name varchar(100),
     amount       int,
     FOREIGN KEY (customer_id) references CUSTOMERS.customers (id)
-);
+    );
