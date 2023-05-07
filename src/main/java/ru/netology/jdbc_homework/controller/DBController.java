33d7c487f0d8c0d0dcfe0080ecc344b358971851
Boris Andros <boris.andros96@gmail.com>
2023-05-07 18:09:48 +0300

    Исправлена реализация

--- src/main/java/ru/netology/jdbc_homework/controller/DBController.java
+++ src/main/java/ru/netology/jdbc_homework/controller/DBController.java
@@ -1,6 +1,5 @@
 package ru.netology.jdbc_homework.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +9,14 @@ import java.util.List;
 
 @RestController
 public class DBController {
-    @Autowired
-    private JdbcRepository repository;
+    private final JdbcRepository repository;
+
+    public DBController(JdbcRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/products/fetch-product")
-    public List<String> getProductName (@RequestParam String name){
+    public List<String> getProductName(@RequestParam String name) {
         return repository.getProductName(name);
     }
 }
